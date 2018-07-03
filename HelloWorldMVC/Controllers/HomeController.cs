8971468f3f8c84d0520851ffe24b5864155cd16f
Rob Wood <rdrwood@gmail.com>
2018-07-03 11:35:33 -0700

    Add project files.

---
+++ HelloWorldMVC/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+            // Response.Write("Welcome to the index!"); // example code to explain the controllers
+        }
+
+        public void Rob()
+        {
+            Response.Write("BREAK TIME");
+        }
+    }
+}
