125f40acd7be67c046fa74e89e29f03e18ebf39f
Rob Wood <rdrwood@gmail.com>
2018-07-05 11:31:38 -0700

    more asp work

--- HelloWorldMVC/Controllers/HomeController.cs
+++ HelloWorldMVC/Controllers/HomeController.cs
@@ -11,13 +11,38 @@ namespace HelloWorldMVC.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            DayOfWeek day = DateTime.Today.DayOfWeek;
+            if (day == DayOfWeek.Thursday)
+            {
+                // viewbag is a wrapper around ViewData
+                ViewBag.Message = "It's Thursday!";
+                // same as above
+                ViewData["Message"] = "It's Thursday!";
+
+            }
+            else {
+                ViewBag.Message = "Not Thursday!";
+            }
+            // data inside viewbag or ViewData is auto sent to the view
+
             return View();
             // Response.Write("Welcome to the index!"); // example code to explain the controllers
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+            // will return the contact page
         }
 
         public void Rob()
         {
             Response.Write("BREAK TIME");
+        }
+
+        public ActionResult About()
+        {
+            return View();
         }
     }
 }
