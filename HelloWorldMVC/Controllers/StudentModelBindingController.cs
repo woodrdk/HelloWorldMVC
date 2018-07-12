e6776c892b8895e3bc0a8428c553f3e8637f98fa
Rob Wood <rdrwood@gmail.com>
2018-07-12 11:19:21 -0700

    model binding version form

---
+++ HelloWorldMVC/Controllers/StudentModelBindingController.cs
@@ -0,0 +1,35 @@
+﻿using HelloWorldMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class StudentModelBindingController : Controller
+    {
+        // GET: StudentModelBinding/create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Student stu)
+        {
+            // this ensures all validation rules for the model have passed
+            if (ModelState.IsValid)
+            {
+                if (StudentDB.AddStudent(stu))
+                {
+                    ViewBag.Added = true;
+                }
+            }
+            return View();
+        }
+
+
+    }
+}
--- HelloWorldMVC/Models/Student.cs
+++ HelloWorldMVC/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,23 @@ namespace HelloWorldMVC.Models
 {
     public class Student
     {
+        [Required(ErrorMessage = "ID Required")]
+        [Display(Name = "Student Id")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "SID must be 9 digits")]
         public string StudentId { get; set; }
+
+        [Required(ErrorMessage = "First Name Required")] 
+        [Display(Name = "First Name")]
+        [StringLength(25, ErrorMessage = "This must be 1 - 25 characters")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name Required")]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "This must be 1 - 50 characters")]
         public string LastName { get; set; }
+
+        // value types are implicitly required because they have to have a value
+        [Display(Name = "Date of birth")]
         public DateTime DateOfBirth { get; set; }
     }
 }
